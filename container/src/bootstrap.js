9f5a513bd42ac968bc5ff24ca996ed067a88862c
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-05-06 10:28:17 -0300

    refactoring app for better integration and code reuse

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as cartMount } from 'cart/CartShow';
+import { mount as productsMount } from '../../products/src/bootstrap';
 
 console.log('container');
+
+productsMount(document.querySelector('#container-products'));
+cartMount(document.querySelector('#container-cart'));
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,11 +1,25 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (element) => {
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
 
-  products += `<div>${name}</div>`;
+    products += `<div>${name}</div>`;
+  }
+
+  element.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const element = document.querySelector('#dev-products');
+
+  //assuming the container doesn't have an element with id 'dev-products'
+  if (element) {
+    //so, running in isolation
+    mount(element);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+export { mount };
